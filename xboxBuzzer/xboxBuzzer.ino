c7664825494587c25e2487c4aabfdceed4a50071
Bruno Landim Toledo <88839468+brult32@users.noreply.github.com>
2024-07-04 19:44:11 -0300

    Update xboxBuzzer.ino

--- xboxBuzzer/xboxBuzzer.ino
+++ xboxBuzzer/xboxBuzzer.ino
@@ -1,18 +1,18 @@
-long timeOut = 30000;  //10min 600000
-const long quarterTime = timeOut / 4;
+unsigned long onePercent = 300;
+unsigned long timeOut = onePercent * 100;  //10min 600000
 const long extraTime = timeOut;  //10min 600000
-int extraCounter = -1;  //10min 600000
+int extraCounter = -1;           //10min 600000
 unsigned long previousMillis = 0;
 unsigned long interval = 1000;
 
 const int buzzerPin = 9;
 const int ledInterval = 8;
-int ledTimerPins[] = {2, 3, 4, 5};
-int ledExtraPins[] = {6, 7};
+int ledTimerPins[] = { 2, 3, 4, 5 };
+int ledExtraPins[] = { 6, 7 };
 
 int loopAllLeds = 3;
-int ledCounter[] = {quarterTime, quarterTime * 2, quarterTime * 3 , (timeOut / 10) * 9};
-bool ledTimerPassed[] = {false, false, false, false};
+unsigned long ledCounter[] = { onePercent * 25, onePercent * 50, onePercent * 75, onePercent * 90 };
+bool ledTimerPassed[] = { false, false, false, false };
 
 int ledState = HIGH;
 
@@ -21,27 +21,32 @@ int butResetState = 0;
 
 const int buttonChangeTimePin = 11;
 int buttonChangeTimeState = 0;
+bool buttonChangeTimePressed = false;
 
 const int buttonStopNoisePin = 12;
 int buttonStopNoiseState = 0;
-bool stopNoise = false;
+bool playNoise = false;
+bool doneTime = false;
 
-int melody[] = {261.63, 196, 196, 220, 196, 0, 247.94, 261.63};
-int noteDurations[] = {4, 8, 8, 4, 4, 4, 4, 4};
+float melody[] = { 261.63, 196, 196, 220, 196, 0, 247.94, 261.63 };
+int noteDurations[] = { 4, 8, 8, 4, 4, 4, 4, 4 };
 
 void setup() {
   pinMode(buzzerPin, OUTPUT);
   pinMode(ledInterval, OUTPUT);
   pinMode(buttonResetPin, INPUT);
   pinMode(buttonStopNoisePin, INPUT);
-  for(int i = 0; i < loopAllLeds; i++){
+  for (int i = 0; i < loopAllLeds; i++) {
     pinMode(ledTimerPins[i], OUTPUT);
-  } 
-  pinMode(ledExtraPins[0], OUTPUT);
-  pinMode(ledExtraPins[1], OUTPUT);
+    digitalWrite(ledTimerPins[i], LOW);
+  }
+  for (int i = 0; i < 2; i++) {
+    pinMode(ledExtraPins[1], OUTPUT);
+    digitalWrite(ledExtraPins[0], LOW);
+  }
 }
 
-void playBuzz(){
+void playBuzz() {
   for (int thisNote = 0; thisNote < 8; thisNote++) {
     int noteDuration = 1000 / noteDurations[thisNote];
     tone(buzzerPin, melody[thisNote], noteDuration);
@@ -53,31 +58,43 @@ void playBuzz(){
 
 void (*resetFunc)(void) = 0;  //declare reset function @ address 0
 
-void loop() {
-  unsigned long currentMillis = millis();
-  if (currentMillis >= timeOut) {
-    if (!stopNoise){
-      playBuzz();
-    }    
+void timeEnd(){
+if (!doneTime) {
+      playNoise = true;
+      doneTime = true;
+    }
+}
+void afterEnd(){
+  if (playNoise) {
+    playBuzz();
+  }
+  if (doneTime){
     buttonStopNoiseState = digitalRead(buttonStopNoisePin);
     if (buttonStopNoiseState == HIGH) {
       digitalWrite(buzzerPin, LOW);
-      stopNoise = true;
+      playNoise = false;
     }
     butResetState = digitalRead(buttonResetPin);
     if (butResetState == HIGH) {
       Serial.println("resetting");
       resetFunc();  //call reset
     }
-    
+  }
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  afterEnd();
+  if (currentMillis >= timeOut) {
+    timeEnd();
   } else {
-    for (int i = 0; i < loopAllLeds; i++){
-        if (!ledTimerPassed[i] && currentMillis > ledCounter[i]){
-          ledTimerPassed[i] = true;
-          digitalWrite(ledTimerPins[i], HIGH);
-        }
+    for (int i = 0; i <= loopAllLeds; i++) {
+      if (!ledTimerPassed[i] && currentMillis > ledCounter[i]) {
+        ledTimerPassed[i] = true;
+        digitalWrite(ledTimerPins[i], HIGH);
       }
-    if (currentMillis - previousMillis >= interval) {      
+    }
+    if (currentMillis - previousMillis >= interval) {
       previousMillis = currentMillis;
       if (ledState == LOW) {
         ledState = HIGH;
@@ -89,19 +106,24 @@ void loop() {
 
     buttonChangeTimeState = digitalRead(buttonChangeTimePin);
     if (buttonChangeTimeState == HIGH) {
-      timeOut += extraTime;
-      extraCounter++;
-      if(extraCounter >= 2){
-        timeOut = extraTime;
-        extraCounter = -1;
-      }
-      if (extraCounter != 0){
-        digitalWrite(ledExtraPins[extraCounter], HIGH); 
-      }else{
-        for (int i = 1; i < 3; i++){
-          digitalWrite(ledExtraPins[i], LOW); 
+      if (!buttonChangeTimePressed) {
+        buttonChangeTimePressed = true;
+        timeOut += extraTime;
+        extraCounter++;
+        if (extraCounter >= 2) {
+          timeOut = extraTime;
+          extraCounter = -1;
+        }
+        if (extraCounter > -1) {
+          digitalWrite(ledExtraPins[extraCounter], HIGH);
+        } else {
+          for (int i = 0; i < 2; i++) {
+            digitalWrite(ledExtraPins[i], LOW);
+          }
         }
       }
+    }else {
+      buttonChangeTimePressed = false;
     }
   }
 }
